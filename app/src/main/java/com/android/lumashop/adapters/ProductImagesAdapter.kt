20684fa3eacf7e26a07c0ec36362619e6cd552ea
Pravini Wick <pravinipw15@gmail.com>
2024-10-08 22:51:16 +0530

    Modified Product Images Adaptor

--- app/src/main/java/com/android/lumashop/adapters/ProductImagesAdapter.kt
+++ app/src/main/java/com/android/lumashop/adapters/ProductImagesAdapter.kt
@@ -7,17 +7,21 @@ import com.android.lumashop.databinding.ItemProductImageBinding
 
 class ProductImagesAdapter(private val images: List<Int>) : RecyclerView.Adapter<ProductImagesAdapter.ProductImageViewHolder>() {
 
+    // Inflate item layout and create ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductImageViewHolder {
         val binding = ItemProductImageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ProductImageViewHolder(binding)
     }
 
+    // Bind image resource to the ImageView
     override fun onBindViewHolder(holder: ProductImageViewHolder, position: Int) {
         val imageResId = images[position]
         holder.binding.imageView.setImageResource(imageResId)
     }
 
+    // Return the total number of images
     override fun getItemCount() = images.size
 
+    // ViewHolder class for the product image
     inner class ProductImageViewHolder(val binding: ItemProductImageBinding) : RecyclerView.ViewHolder(binding.root)
 }
